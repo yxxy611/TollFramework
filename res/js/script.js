b60b1ed9a05ece4f07d207a146f58680b1bb2386
Fossia <yxxy611@hotmail.com>
2017-04-11 23:16:14 +0800

    implement mouse over pick sort codes

--- res/js/script.js
+++ res/js/script.js
@@ -7,11 +7,14 @@ function start() {
 
     var tollgates = [];
     var cctvs = [];
+    var mes = 123;
 
     var scene = new THREE.Scene();
     var renderer = new THREE.WebGLRenderer();
+    var mouse = new THREE.Vector2(), INTERSECTED;
+    var raycaster = new THREE.Raycaster();;
+
     renderer.setPixelRatio(window.devicePixelRatio);
-    console.log(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor(0xaaaaaa);
     document.body.appendChild(renderer.domElement);
@@ -28,32 +31,80 @@ function start() {
     controls.maxDistance = 7500;
     //light
     var ambient = new THREE.AmbientLight(0x666666, 0.5);
-    scene.add(ambient);
     var light = new THREE.DirectionalLight(0xffffff, 1);
     light.position.set(1, 0, 1);
+    scene.add(ambient);
     scene.add(light);
     //load manager
     var manager = new THREE.LoadingManager();
     manager.onProgress = function (item, loaded, total) {
         console.log(item, loaded, total);
     };
+    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+    window.addEventListener( 'resize', onWindowResize, false );
 
     //ar layoutDesigner = new LayoutDesigner(scene);
 
     function initLayout() {
-        var gate = new TollGate(manager,scene);
+        //var gate = new ObjComponent();
+        var gate1 = new TollGate(scene,manager);
+        gate1.setPos(0,0,0);
+        gate1.updateMesh();
         //layoutDesigner.addTollGate(gate);
-        scene.add(gate);
+
+
     }
-
+    function animate() {
+        requestAnimationFrame( animate );
+        render();
+    }
     function render() {
+        // find intersections
+        raycaster.setFromCamera( mouse, camera );
 
-        requestAnimationFrame(render);
+        var intersects = raycaster.intersectObjects( scene.children );
+//        console.log("intersects"+" "+intersects.length);
+//        console.log("    "+" ");
+        if ( intersects.length > 0 ) {
+
+            if ( INTERSECTED != intersects[ 0 ].object ) {
+
+                if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+
+                INTERSECTED = intersects[ 0 ].object;
+                INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+                INTERSECTED.material.emissive.setHex( 0xff0000 );
+
+            }
+
+        }
+        else {
+
+            if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+
+            INTERSECTED = null;
+
+        }
+
         renderer.render(scene, camera);
 
     };
+    function onWindowResize() {
+        windowHalfX = window.innerWidth / 2;
+        windowHalfY = window.innerHeight / 2;
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+    }
+    function onDocumentMouseMove( event ) {
+
+        event.preventDefault();
+
+        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        mouse.y = -( event.clientY / window.innerHeight ) * 2 + 1;
+    }
 
     initLayout();
-    render();
+    animate();
 }
 window.onload = start;
